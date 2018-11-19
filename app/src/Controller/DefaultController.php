db65e8c71f782021a0e3201a90bc39ff433e4d01
Uday Shiwakoti <uday.shiwakoti@mullenlowegroup.com>
2018-11-19 12:08:01 +0000

    Paginator added

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -10,10 +13,24 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(ParameterBagInterface $params,ContainerInterface $ci,Request $request)
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+        #https://github.com/KnpLabs/KnpPaginatorBundle
+        $data=[];
+        for($i=0;$i<100;$i++){
+            $data[]=['id'=>$i,'title'=>'uday','date'=>date('Y-m-d H:i:s')];
+        }
+
+        $paginator  = $ci->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $data, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            10/*limit per page*/
+        );
+
+
+        return $this->render('paginator/index.html.twig', [
+            'pagination'=>$pagination
         ]);
     }
 }
